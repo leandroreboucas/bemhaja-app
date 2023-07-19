189fc5cbde85a256ef148305d9aa8b78d1cad600
Leandro Rebouças <contato@leandroreboucas.com>
2023-07-19 16:18:14 -0300

    chore: fontes da aplicaçao

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import {
+  useFonts,
+  Roboto_300Light,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import {
+  Poppins_300Light,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Poppins_300Light,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
